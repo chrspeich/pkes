334d34e6a2fadfc8060137119a4e731ec429c13b
kTonscheidt <ktonscheidt@web.de>
2015-01-14 10:03:34 +0100

    created skeleton for move class

---
+++ Aufgabe4/move.cpp
@@ -0,0 +1,17 @@
+#include "move.h"
+
+
+void Move::rotate(int degrees)
+{
+  
+  
+  
+}
+
+void Move::drive(int distance)
+{
+  
+  
+  
+  
+}
---
+++ Aufgabe4/move.h
@@ -0,0 +1,13 @@
+#include "fly_Sensor.h"
+
+class Move
+{
+  public:
+  
+    void rotate(int degrees);
+    
+    /**
+    * distance in cm 
+    */
+    void drive(int distance);
+};
